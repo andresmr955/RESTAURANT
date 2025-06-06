b689f4f0d980c262b5eefdf0c7d63877c5d23047
Andres Marquez <andresmr955@gmail.com>
2025-06-06 11:29:41 -0400

    serving photos in my url main

--- restaurantapp/productivity_restaurant/urls.py
+++ restaurantapp/productivity_restaurant/urls.py
@@ -24,3 +24,7 @@ urlpatterns = [
     path('employee/dashboard/', views.employee_dashboard, name='employee_dashboard'),
     path('logout/', views.logout_view, name='logout'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
