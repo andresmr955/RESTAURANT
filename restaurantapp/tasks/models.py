18849060a2112c6025f840b05da8b3d133f11dd5
Andres Marquez <andresmr955@gmail.com>
2025-06-06 11:53:45 -0400

    creating tasks model and making migrations

--- restaurantapp/tasks/models.py
+++ restaurantapp/tasks/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from django.conf import settings
 # Create your models here.
+
+class Task(models.Model):
+
+    STATUS_CHOICES = [
+            ('pending', 'Pending'),
+            ('in progress', 'In Progress'), 
+            ('completed', 'Completed')
+        ]
+    description = models.CharField(max_length=255)
+    assigned_employee = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, 
+        related_name = 'tasks'
+    )
+
+    priority = models.IntegerField(default=1)
+    assigned_date = models.DateTimeField(auto_now_add=True)
+    start_time = models.DateTimeField(blank=True, null=True)
+    end_time = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(
+        max_length=20, 
+        choices=STATUS_CHOICES, 
+        default = 'pending'
+    )
+
+    comments = models.TextField(blank=True, null=True)
+
+    def total_time(self):
+        if self.start_time and self.end_time:
+            return (self.end_time - self.start_time) / 60
+        return None
+
+    def __str__(self):
+        return f"{Self.description} for {self.assigned_employee.username}"
+    
