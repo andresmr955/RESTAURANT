416e8f39f9d3c74c376d6fa4942f99799ee8d185
Andres Marquez <andresmr955@gmail.com>
2025-06-06 11:19:59 -0400

    db setup

--- restaurantapp/productivity_restaurant/settings.py
+++ restaurantapp/productivity_restaurant/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
-        'HOST': 'config('DB_HOST', default='localhost'),
-        'PORT': 'config('DB_PORT', default='5432'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default='5432'),
 
     }
 }
