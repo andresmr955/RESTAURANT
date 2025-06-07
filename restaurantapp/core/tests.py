37a405f5eaae20375dc043ae09587f0c81862ff8
Andres Marquez <andresmr955@gmail.com>
2025-06-07 12:18:18 -0400

    creating test for core views

--- restaurantapp/core/tests.py
+++ restaurantapp/core/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
-
+from django.urls import reverse
 # Create your tests here.
+
+
+class HomeViewTests(TestCase):
+    def test_home_view_status_code(self):
+        response = self.client.get(reverse('core:home'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'base.html')
