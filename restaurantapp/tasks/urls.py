d05ed1f5a6b2dcb54fe01c32a88377357f56597b
Andres Marquez <andresmr955@gmail.com>
2025-06-07 12:02:41 -0400

    creating view after login

--- restaurantapp/tasks/urls.py
+++ restaurantapp/tasks/urls.py
@@ -2,12 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings    
 from django.conf.urls.static import static
-from .views import TaskCreateView
+from .views import TaskCreateView, TaskListView
 
 app_name = 'tasks'
 
 urlpatterns = [
-    path('create/', TaskCreateView.as_view(), name='create_task')
+    path('create/', TaskCreateView.as_view(), name='create_task'),
+    path('tasks/', TaskListView.as_view(), name='task_list')
     
 ]
 
--- restaurantapp/tasks/views.py
+++ restaurantapp/tasks/views.py
@@ -1,9 +1,10 @@
 # tasks/views.py
 from django.urls import reverse_lazy
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
-from django.views.generic import CreateView
+from django.views.generic import CreateView, ListView
 from .models import Task
 from .forms import TaskForm
+
 
 class TaskCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Task
@@ -20,3 +21,10 @@ class TaskCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     def form_valid(self, form):
        
         return super().form_valid(form)
+
+
+class TaskListView(ListView):
+
+    model = Task
+    template_name = 'tasks/task_list.html'
+    context_object_name = 'tasks'
