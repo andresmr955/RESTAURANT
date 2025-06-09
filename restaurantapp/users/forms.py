c616b2a1a65d60067188bc18b012ba2e455ee1b6
Andres Marquez <andresmr955@gmail.com>
2025-06-09 08:05:25 -0400

    creating form for add employees

--- restaurantapp/users/forms.py
+++ restaurantapp/users/forms.py
@@ -1,6 +1,23 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import CustomerUser
+
 
 class LoginForm(AuthenticationForm):
     username = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={"class": 'form-control'}))
+
+class EmployeeForm(forms.ModelForm):
+
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
+
+    class Meta:
+        model = CustomerUser
+        fields = ['username', 'email', 'first_name', 'last_name', 'role', 'phone_number', 'avatar', 'date_birth', 'address', 'notifications_enabled']
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        user.set_password(self.cleaned_data['password'])
+        if commit:
+            user.save()
+        return user
--- restaurantapp/users/views.py
+++ restaurantapp/users/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render, redirect
-from datetime import datetime
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.decorators import login_required
-from django.utils.timezone import now
-from django.http import JsonResponse
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.mixins import UserPassesTestMixin
+from .models import CustomerUser
+from .forms import EmployeeForm
+
+class EmployeeCreateView(UserPassesTestMixin, CreateView):
+    model = CustomerUser
+    form_class = EmployeeForm
+    template_name = 'users/add_employee.html'
+    success_url = reverse_lazy('employee_list')
+
+    def test_func(self):
+        return self.request.user.is_authenticated and self.request.user.is_manager()
