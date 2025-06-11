733206493274aeb2f35f69f6beefb48d013eb820
Andres Marquez <andresmr955@gmail.com>
2025-06-11 11:43:23 -0400

    refactor to all app to better read the code

--- restaurantapp/users/urls.py
+++ restaurantapp/users/urls.py
@@ -17,13 +17,14 @@ app_name = 'users'
 urlpatterns = [
    
     #URLS TO PROVE MY MODELS
-    # path('login/', CustomLoginView.as_view(template_name="users/login.html") , name="login"),
+    path('login/', CustomLoginView.as_view(template_name="users/login.html") , name="login"),
     path('logout/', auth_views.LogoutView.as_view(next_page="users:login"), name="logout"),
     path('add-employee/', EmployeeCreateView.as_view(), name="add-employee"),
     path('employee-list/', EmployeeList.as_view(), name="employee_list" ),
     
+
+
     ##APIS FOR MY FRONTEND
-    
     
     path("loginjwt/",   LoginView.as_view(),  name="jwt_login"),   # POST
     # http://127.0.0.1:8000/api/auth/loginjwt/
