00e8605177bc4063ae56162a6e6e00fef3e38116
Andres Marquez <andresmr955@gmail.com>
2025-06-12 14:55:37 -0400

    making translate

--- restaurantapp/users/serializers.py
+++ restaurantapp/users/serializers.py
@@ -9,31 +9,31 @@ class CustomTokenSerializer(TokenObtainPairSerializer):
 
     @classmethod
     def get_token(cls, user):
-        # Obtiene el token base y agrega información adicional al payload
+        # Gets the base token and adds additional information to the payload
         token = super().get_token(user)
-        token["username"] = user.username  # Incluir el 'username' en el token
-        token["email"] = user.email  # Incluir el email en el token
-        token["is_manager"] = user.is_manager()  # Agregar otros campos relevantes si es necesario
+        token["username"] = user.username  # Include the 'username' in the token
+        token["email"] = user.email  # Include the email in the token
+        token["is_manager"] = user.is_manager()  # Add other relevant fields if necessary
         return token
 
     def validate(self, attrs):
-        email = attrs.get("email")  # Usamos 'email' aquí
+        email = attrs.get("email")  # We use 'email' here
 
         password = attrs.get("password")
         if not email:
-           raise exceptions.AuthenticationFailed("Email is required")
-        # Buscar al usuario por email
+            raise exceptions.AuthenticationFailed("Email is required")
+        # Find the user by email
         try:
-            user = CustomerUser.objects.get(email=email)  # Buscar al usuario por email
+            user = CustomerUser.objects.get(email=email)  # Find the user by email
         except CustomerUser.DoesNotExist:
             raise exceptions.AuthenticationFailed("No user found with this email")
 
-        # Realizar autenticación con 'username', ya que es lo que usa Django por defecto
-        user = authenticate(username=user.email, password=password)  # Usar 'username' para autenticar
+        # Perform authentication with 'username', as this is what Django uses by default
+        user = authenticate(username=user.email, password=password)  # Use 'username' to authenticate
         if not user:
             raise exceptions.AuthenticationFailed("Invalid credentials")
 
-        # Si todo está bien, generamos el token
+        # If everything is okay, we generate the token
         return super().validate(attrs)
 
 class EmployeeSerializer(serializers.ModelSerializer):
@@ -63,10 +63,10 @@ class EmployeeCreateSerializer(serializers.ModelSerializer):
         extra_kwargs = {
             'password': {'write_only': True}
         }
-    
+
     def create(self, validated_data):
-        # Usa create_user si tienes un método que maneje hashing de contraseña
+        # Use create_user if you have a method that handles password hashing
         validated_data['username'] = validated_data['email'].split('@')[0]
-        
+
         user = CustomerUser.objects.create_user(**validated_data)
         return user
