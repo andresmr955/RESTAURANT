ceda4ed9baf38c761222ed6880c9ae1e2df89fc8
Andres Marquez <andresmr955@gmail.com>
2025-06-12 14:05:23 -0400

    deleting typescript and jscript becase we have the frontend in other directory

--- restaurantapp/productivity_restaurant/urls.py
+++ restaurantapp/productivity_restaurant/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from users import views
 from django.conf import settings
 from django.conf.urls.static import static
+
+from users import views
 
 urlpatterns = [
    
