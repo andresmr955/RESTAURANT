f0320778f955d341f15491940c2532e0d6e686d1
Andres Marquez <andresmr955@gmail.com>
2025-06-12 13:56:41 -0400

    cleaning attendance app

--- restaurantapp/attendance/urls.py
+++ restaurantapp/attendance/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
-from .views import HelloWorld, start_attendance, stop_attendance, attendance_page
+
 app_name = 'attendance' 
 
 urlpatterns = [
-    path('start/', start_attendance, name='start_attendance'),
-    path('stop/', stop_attendance, name='stop_attendance'),
-    path('', attendance_page, name='attendance_page'),
-     path('api/hello/', HelloWorld.as_view(), name='hello_world')
+   
 ]
