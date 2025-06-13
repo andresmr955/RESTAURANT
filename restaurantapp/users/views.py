4241d625a0f80aa92d09492ec8ae478ae119bcba
Andres Marquez <andresmr955@gmail.com>
2025-06-13 08:15:11 -0400

    creating users viewsets

--- restaurantapp/users/views.py
+++ restaurantapp/users/views.py
@@ -2,16 +2,8 @@ from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import UserPassesTestMixin
 
-
-from rest_framework.generics import ( ListCreateAPIView, RetrieveUpdateDestroyAPIView)
-
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.exceptions import PermissionDenied
-
-from drf_spectacular.utils import extend_schema
-
 from .models import CustomerUser
-from .serializers import EmployeeSerializer, EmployeeCreateSerializer
+
 from .forms import EmployeeForm
 
 
@@ -29,8 +21,6 @@ class EmployeeCreateView(UserPassesTestMixin, CreateView):
         return super().form_invalid(form)
 
 
-
-
 class EmployeeList(UserPassesTestMixin, ListView):
     model = CustomerUser
     template_name = 'users/employee_list.html'
@@ -41,39 +31,3 @@ class EmployeeList(UserPassesTestMixin, ListView):
     
     def get_queryset(self):
         return CustomerUser.objects.exclude(role='admin')
-
-@extend_schema(tags=["Users"])
-class EmployeeListCreateAPI(ListCreateAPIView):
-    """
-    Get the list of employees or create employees
-    """
-    permission_classes = [IsAuthenticated]
-    queryset = CustomerUser.objects.all()
-
-    def get_serializer_class(self):
-        if self.request.method == 'POST':
-            return EmployeeCreateSerializer
-        return EmployeeSerializer
-
-    def perform_create(self, serializer):
-        if not self.request.user.is_manager():
-            raise PermissionDenied("Only managers can create employees")
-        serializer.save()
-
-@extend_schema(tags=["Users"])
-class EmployeeDetailUpdateAPI(RetrieveUpdateDestroyAPIView):
-    queryset = CustomerUser.objects.all()
-    serializer_class = EmployeeSerializer
-    permission_classes = [IsAuthenticated]
-    lookup_field = 'pk' 
-
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_manager():
-            return CustomerUser.objects.all()
-        return CustomerUser.objects.none()
-
-    def destroy(self, request, *args, **kwargs):
-        if not request.user.is_manager():
-            raise PermissionDenied("Just managers can delete employees")
-        return super().destroy(request, *args, **kwargs)
--- restaurantapp/users/viewsets.py
+++ restaurantapp/users/viewsets.py
@@ -1,3 +1,44 @@
-from rest_framework import viewsets
+from drf_spectacular.utils import extend_schema
+from rest_framework import viewsets, permissions
 
-class 
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.exceptions import PermissionDenied
+
+from .models import CustomerUser
+from .serializers import EmployeeSerializer, EmployeeCreateSerializer
+
+@extend_schema(tags=["Users"])
+class EmployeeViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet to take actions with employees:
+    List, create, update, and delete
+
+    """
+    queryset = CustomerUser.objects.all()
+    permission_classes = [IsAuthenticated]
+    lookupfield = 'pk'
+
+    def get_serializer_class(self):
+        if self.action == 'create':
+            return EmployeeCreateSerializer
+        return EmployeeSerializer
+
+    def get_queryset(self):
+        """
+        Filter according to the users role that makes the consult
+        """
+
+        user = self.request.user
+        if user.is_manager():
+            return CustomerUser.objects.all()
+        return CustomerUser.objects.none()
+
+    def perform_create(self, serializer):
+        if not self.request.user.is_manager():
+            raise PermissionDenied("Only managers can crete employees")
+        serializer.save()
+
+    def destroy(self, request, *args, **kwargs):
+        if not request.user.is_manager():
+            raise PermissionDenied("Just managers can delete employees")
+        return super().destroy(request, *args, **kwargs)
