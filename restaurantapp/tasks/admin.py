e1d5963c5ba39377a33733dc7d63c0525cd42057
Andres Marquez <andresmr955@gmail.com>
2025-06-13 11:32:27 -0400

    modifying task admin.py to hceck in django the task in order of date assigned

--- restaurantapp/tasks/admin.py
+++ restaurantapp/tasks/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Task
 
 # Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('assigned_employee','id', 'status', 'description', 'priority', 'assigned_date')
+    search_fields = ('description', 'assigned_employee__email')
+    list_filter = ('status', 'priority', 'assigned_date')
+    ordering = ('assigned_date',)
