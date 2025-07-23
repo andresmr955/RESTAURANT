48370f73c619cecc17381e46754d6c5f4bfc8bc3
Andres Marquez <andresmr955@gmail.com>
2025-07-23 16:43:47 -0400

    creating the page who is going to show the employees

--- frontend/src/app/core/services/employees-list.ts
+++ frontend/src/app/core/services/employees-list.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
 import { CustomerUser } from './../../models/user.model';
+import { Observable } from 'rxjs';
+import { checkToken } from '../interceptors/token-interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +14,9 @@ export class EmployeesList {
 
   constructor(private http: HttpClient) { }
 
-  
+  getAllEmployeesList(): Observable<CustomerUser[]>{
+    return this.http.get<any[]>(`${this.apiUrl}/api/users/employees-actions/`,  {
+          context: checkToken()
+        })
+  }
 }
