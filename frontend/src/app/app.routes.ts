2511d54cdc9ec4dd6fb35605cc4152793ce164be
Andres Marquez <andresmr955@gmail.com>
2025-07-23 11:23:26 -0400

    adding the dashboard

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,17 +1,17 @@
 import { Routes } from '@angular/router';
-import { Login } from './modules/auth/pages/login/login';
-import { EmployeeTasks } from './modules/employees_tasks/pages/employee-tasks/employee-tasks';
+import { Login } from './domains/auth/pages/login/login';
+import { EmployeeTasks } from './domains/employees_tasks/pages/employee-tasks/employee-tasks';
 import { NotFoundComponent } from './shared/components/not-found-component/not-found-component';
 export const routes: Routes = [
      {
     path: '',
-    loadChildren: () => import('./modules/auth/auth-module').then((m) => m.AuthModule),
+    loadChildren: () => import('./domains/auth/auth-module').then((m) => m.AuthModule),
     },
     {
         path: 'employee-tasks', 
         component: EmployeeTasks
     },
-    {
+    {   
         path: '**',
         component: NotFoundComponent
     }
